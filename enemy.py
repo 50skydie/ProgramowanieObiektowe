0cac0fcaaa600775136ac75681e5bbba29b382ab
dieplsky <dieplsky@gmail.com>
2020-05-03 20:36:41 +0200

    added simple enemy class, no inheritance yet, tmp images

--- enemy.py
+++ enemy.py
@@ -1,1 +1,26 @@
-import pygame as pg
+import pygame
+import sys
+from pygame.locals import *
+from random import *
+
+class Enemy:
+    def __init__(self):
+        self._enemy_image = pygame.image.load("spritesheets/enemy1.png")
+        self._speed = 1
+        self._points = 25
+        self._x_pos = 1280
+        self._y_pos = randrange(0, 620)
+        self._starting_pos = (self._x_pos, self._y_pos) # generating enemy at max right and random height
+    
+    def renderEnemy(self, display_surf):
+        display_surf.blit(pygame.transform.scale(self._enemy_image, (200, 100)), self._starting_pos)
+
+    def enemyMovment(self):
+        self._x_pos -= self._speed
+        self._starting_pos = (self._x_pos, self._y_pos) # can this be more optimal?
+
+    def outOfBoundry(self):
+        #pseudo auto generating objects XDDD
+        if(self._x_pos < -500):
+            self._x_pos = 1280
+            self._y_pos = randrange(0, 620)
--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import pygame
 import sys
 from pygame.locals import *
+from enemy import Enemy
+from random import *
 
 class Game:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 1280, 720
+        self._size = self.weight, self.height = 1280, 720
+        self._background_image = None
+        self._num_of_enemies = 1
+        self._enemies = []
+        for i in range(self._num_of_enemies): 
+            self._enemies.append(Enemy())
  
     def on_init(self): #init things, works only once at start
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size) 
+        pygame.display.set_caption("ProgramowanieObiektowe")
+        self._display_surf = pygame.display.set_mode(self._size) 
         self._running = True
+        self._background_image = pygame.image.load("spritesheets/tmp_bg.jpg").convert()
  
     def on_event(self, event): #events here
         if event.type == pygame.QUIT:
             self._running = False
     def on_loop(self): #loop things here
-        pass
+        for i in range(self._num_of_enemies): 
+            self._enemies[i].enemyMovment()
+            self._enemies[i].outOfBoundry()
     def on_render(self): #render thigns here
-        pass
+        self._display_surf.blit(self._background_image, [0,0])
+        for i in range(self._num_of_enemies):
+            self._enemies[i].renderEnemy(self._display_surf) 
+        pygame.display.flip()
     def on_cleanup(self): #just cleaning modules
         pygame.quit()
  
