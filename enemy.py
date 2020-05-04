4e6a38da87304947e559a3c266a49913831f6423
dieplsky <dieplsky@gmail.com>
2020-05-04 23:29:22 +0200

    added: class inheritance, Bird and Crow subclasses of class Enemy, fixed spawning enemies, now it is random

--- enemy.py
+++ enemy.py
@@ -3,9 +3,10 @@ import sys
 from pygame.locals import *
 from random import *
 
+#Parent class
 class Enemy:
     def __init__(self):
-        self._enemy_image = pygame.image.load("spritesheets/enemy1.png")
+        self._enemy_image = None
         self._speed = 1
         self._points = 25
         self._x_pos = 1280
@@ -19,8 +20,37 @@ class Enemy:
         self._x_pos -= self._speed
         self._starting_pos = (self._x_pos, self._y_pos) # can this be more optimal?
 
-    def outOfBoundry(self):
-        #pseudo auto generating objects XDDD
+    def resetPosition(self):
+        self._x_pos = 1280
+        self._y_pos = randrange(0, 620)
+
+    def isOutOfBoundry(self):
         if(self._x_pos < -500):
-            self._x_pos = 1280
-            self._y_pos = randrange(0, 620)
+            return True
+
+#inheritance example
+class Bird(Enemy):
+    def __init__(self):
+        super().__init__()
+        #overdrive attributes
+        self._speed = 0.5
+        self._y_speed = 0.5
+        self._points = 50
+        self._enemy_image = pygame.image.load("spritesheets/enemy1.png")
+    
+    #overdrive method
+    def enemyMovment(self):
+        self._x_pos -= self._speed
+        if(self._y_pos > 620):
+            self._y_speed = -self._y_speed
+        elif(self._y_pos < 0 ):
+            self._y_speed = -self._y_speed
+        self._y_pos += self._y_speed
+        self._starting_pos = (self._x_pos, self._y_pos) # can this be more optimal tho?
+
+#2nd class simple "copy paste" of parent class
+class Crow(Enemy):
+    def __init__(self):
+        super().__init__()
+        #need to change to other img
+        self._enemy_image = pygame.image.load("spritesheets/enemy1.png")
--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import pygame
 import sys
 from pygame.locals import *
-from enemy import Enemy
+from enemy import *
 from random import *
 
+#there are too much comments, but they can be helpful
 class Game:
     def __init__(self):
         self._running = True
         self._display_surf = None
         self._size = self.weight, self.height = 1280, 720
         self._background_image = None
-        self._num_of_enemies = 1
-        self._enemies = []
-        for i in range(self._num_of_enemies): 
-            self._enemies.append(Enemy())
+        self._enemies = [Bird(), Crow()]
+        self._enemy_id = 0
  
     def on_init(self): #init things, works only once at start
         pygame.init()
@@ -25,19 +24,23 @@ class Game:
     def on_event(self, event): #events here
         if event.type == pygame.QUIT:
             self._running = False
+
     def on_loop(self): #loop things here
-        for i in range(self._num_of_enemies): 
-            self._enemies[i].enemyMovment()
-            self._enemies[i].outOfBoundry()
+        self._enemies[self._enemy_id].enemyMovment()
+        if(self._enemies[self._enemy_id].isOutOfBoundry()): #reseting enemies with random class 0 = bird 1 = crow
+            self._enemy_id = (randrange(0,100) % 2)
+            self._enemies[self._enemy_id].resetPosition()
+            print(self._enemy_id) # debug only
+
     def on_render(self): #render thigns here
         self._display_surf.blit(self._background_image, [0,0])
-        for i in range(self._num_of_enemies):
-            self._enemies[i].renderEnemy(self._display_surf) 
+        self._enemies[self._enemy_id].renderEnemy(self._display_surf) 
         pygame.display.flip()
+
     def on_cleanup(self): #just cleaning modules
         pygame.quit()
  
-    def on_execute(self): #starts final game
+    def on_execute(self): 
         if self.on_init() == False:
             self._running = False
  
