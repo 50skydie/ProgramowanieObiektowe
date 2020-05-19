1a77deeb362eb8d115b01eba63b6b521728e633e
dieplsky <dieplsky@gmail.com>
2020-05-19 13:15:47 +0200

    added is game end logic

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player:
         self.pos = Vector2(50,50)
         self.vel = Vector2(0,0)
         self.acc = Vector2(0,0)
+        self.isdead = False
         
     def force(self,Force):
         self.acc += Force
@@ -30,9 +31,15 @@ class Player:
             self.pos.y = 620    
         
     def draw(self,screen):
-        screen.blit(pygame.transform.scale(pygame.image.load("spritesheets/Player.png"), (200, 100)), self.pos) 
+        if(self.isdead):
+            screen.blit(pygame.transform.scale(pygame.image.load("spritesheets/BLANK.png"), (0, 0)), self.pos)
+        else:
+            screen.blit(pygame.transform.scale(pygame.image.load("spritesheets/Player.png"), (200, 100)), self.pos) 
 
     def collision(self,enemy):
         if (enemy._y_pos-100)<self.pos.y<(enemy._y_pos+100):
             if (enemy._x_pos-100)<self.pos.x<(enemy._x_pos+100):
-                print("wykryto kolizje")
+                self.isdead = True
+
+    def isDead(self):
+        return self.isdead
